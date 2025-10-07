98c81a7e65d88e0ae788432c14bf6a167a796cdf
sadegh-shahid <122522207+sadegh-shahid@users.noreply.github.com>
2025-10-07 11:08:10 +0330

    Update Logo.jsx and remove classes of it

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <Link to="/" className="inline-flex items-center justify-center gap-3">
+    <Link to="/" className=" ">
       <img src="/images/Logo.svg" alt="logo" className="w-11 h-11 mx-auto  " />
     </Link>
   );
